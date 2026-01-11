835ee906ef695af720935dfe630b70c2eae552e6
shivesh93212 <0105cd231185@oriental.ac.in>
2026-01-11 17:21:50 +0530

    final

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,12 @@ from database import SessionLocal,Base,engine
 import models
 from auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
-from models import Product,Cart,CartItem,User
+from models import Product,Cart,CartItem,User,Order,OrderItem
 from sqlalchemy.orm import Session
 import shutil
 import uuid
 import os
+from datetime import datetime
 
 app=FastAPI()
 
@@ -235,6 +236,7 @@ def add_item_in_cart(user_id:int,product_id:int,quantity:int=1,db:Session=Depend
     }
 
 #***************************************************************UPDATE CART ITEM********************************************
+
 @app.patch("/cart/item/{item_id}")
 def update_cart(item_id:int,quantity:int,db:Session=Depends(get_db)):
     cart_item=db.query(CartItem).filter(CartItem.id==item_id).first()
@@ -262,5 +264,91 @@ def update_cart(item_id:int,quantity:int,db:Session=Depends(get_db)):
     }
 
 
-    
+    # ====================================================================ORDERS=================================================
 
+    # **********************************************PLACE ORDERS****************************************************************
+
+# ============================ ORDERS ============================
+
+@app.post("/order/place")
+def place_order(user_id: int, db: Session = Depends(get_db)):
+    cart = db.query(Cart).filter(Cart.user_id == user_id).first()
+    if not cart:
+        raise HTTPException(404, "CART NOT FOUND")
+
+    cart_items = db.query(CartItem).filter(CartItem.cart_id == cart.id).all()
+    if not cart_items:
+        raise HTTPException(400, "Cart is Empty")
+
+    total_amount = 0
+
+    for item in cart_items:
+        product = db.query(Product).filter(Product.id == item.product_id).first()
+
+        if item.quantity > product.quantity:
+            raise HTTPException(
+                400,
+                f"Only {product.quantity} items left for {product.name}"
+            )
+
+        total_amount += item.quantity * product.price
+
+    order = Order(
+        user_id=user_id,
+        total_amount=total_amount,
+        status="PLACED",
+        created_at=datetime.utcnow()
+    )
+
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+
+    # ✅ SAVE ORDER ITEMS
+    for item in cart_items:
+        product = db.query(Product).filter(Product.id == item.product_id).first()
+
+        order_item = OrderItem(
+            order_id=order.id,
+            product_id=product.id,
+            quantity=item.quantity,
+            price=product.price
+        )
+
+        product.quantity -= item.quantity
+        db.add(order_item)
+
+    db.query(CartItem).filter(CartItem.cart_id == cart.id).delete()
+    db.commit()
+
+    return {
+        "order_id": order.id,
+        "total": order.total_amount,
+        "status": order.status,
+        "items": order.items
+    }
+
+
+# ************************************************************GRT USER ORDERS***************************************************
+
+@app.get("/order/user/{user_id}")
+def get_user_orders(user_id: int, db: Session = Depends(get_db)):
+    orders = db.query(Order).filter(Order.user_id == user_id).all()
+    return orders
+
+# ****************************************************************GET  ORDERS DETAILS***********************************************
+
+@app.get("/order/details/{order_id}")
+def get_order_details(order_id: int, db: Session = Depends(get_db)):
+    order = db.query(Order).filter(Order.id == order_id).first()
+
+    if order is None:
+        raise HTTPException(status_code=404, detail="Order Not Found")
+
+    return {
+        "order_id": order.id,
+        "total": order.total_amount,
+        "status": order.status,
+        "items": order.items
+    }
+  
--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text,Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Text,Float,DateTime
 from database import Base
 from sqlalchemy.orm import relationship
-
+from datetime import datetime
 # ================= USER TABLE =================
 
 class User(Base):
@@ -53,3 +53,26 @@ class CartItem(Base):
     cart = relationship("Cart", back_populates="items")
     product = relationship("Product")
 
+class Order(Base):
+    __tablename__="orders"
+
+    id=Column(Integer,primary_key=True,index=True)
+    user_id=Column(Integer,ForeignKey("user.id"))
+    total_amount=Column(Integer)
+    status=Column(String,default="PLACED")
+    created_at=Column(DateTime,default=datetime.utcnow)
+
+    items=relationship("OrderItem",back_populates="order")
+
+class OrderItem(Base):
+    __tablename__="order_items"
+
+    id=Column(Integer,primary_key=True,index=True)
+    order_id=Column(Integer,ForeignKey("orders.id"))
+    product_id=Column(Integer,ForeignKey("product.id"))
+    quantity=Column(Integer)
+    price=Column(Integer)
+
+    order=relationship("Order",back_populates="items")
+
+    
