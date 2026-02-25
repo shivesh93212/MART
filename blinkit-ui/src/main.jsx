5c2cc7b393dc3ed77266c14185d961f1086f3bc5
shivesh93212 <0105cd231185@oriental.ac.in>
2026-02-25 18:17:59 +0530

    final

--- blinkit-ui/src/main.jsx
+++ blinkit-ui/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext.jsx"; // ✅ FIXED
+import { CartProvider } from "./context/CartContext.jsx";
 import {SearchProvider} from "./context/SearchContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
